646caeea1e663d1d9606ff9937aea1ee5860fdde
Kaylen Cairns <kaylen.cairns@gmail.com>
2025-09-03 20:30:51 +0200

    Added a basic header component, nothing special

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import ProjectCard from './components/ProjectCard';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <ProjectCard
-        image="https://via.placeholder.com/150"
-        title="Project Title"
-        description="Project description goes here."
-        badges={['React', 'JavaScript']}
-        websiteUrl="https://example.com"
-        githubUrl="https://github.com"
-      />
-    </div>
-  );
+	return (
+		<div className='bg-blue-950 h-full min-h-screen text-white justify-center items-center'>
+			<Header />
+			<ProjectCard
+				image='https://via.placeholder.com/150'
+				title='Project Title'
+				description='Project description goes here.'
+				badges={['React', 'JavaScript']}
+				websiteUrl='https://example.com'
+				githubUrl='https://github.com'
+			/>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-gray-900 text-white shadow-md">
+            <div className="container mx-auto flex items-center justify-between py-4 px-6">
+                <div className="text-2xl font-bold">
+                    CairnsForge
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
