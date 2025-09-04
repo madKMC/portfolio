732cddb6488c17073a882f2f908de5db454c636a
Kaylen Cairns <kaylen.cairns@gmail.com>
2025-09-04 14:53:41 +0200

    feat: enhance ProjectCard layout with responsive width adjustments and padding

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -16,11 +16,11 @@ const ProjectCard = ({
 	websiteUrl,
 	githubUrl,
 }) => (
-	<div className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl md:min-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
+	<div className='flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:min-w-xl md:max-w-xl lg:min-w-6xl lg:max-w-6xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700'>
 		<img
 			src={image}
 			alt={title}
-			className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg'
+			className='object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg p-2'
 		/>
 		<div className='flex flex-col justify-between p-4 leading-normal'>
 			<h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
